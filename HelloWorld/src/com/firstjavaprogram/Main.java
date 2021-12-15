76ab3b6c899694e37e1d22327aafb0eb181fc2e4
npranger <96153359+npranger@users.noreply.github.com>
2021-12-14 18:38:00 -0600

    First Commit

---
+++ HelloWorld/src/com/firstjavaprogram/Main.java
@@ -0,0 +1,46 @@
+package com.firstjavaprogram;
+
+import java.awt.*;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Mortgage Calculator
+        //Source documentation: https://www.wikihow.com/Calculate-Mortgage-Payments
+
+        //Declaring constant variables
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        //Scanner begins reading user input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        //Mortgage calculation (See source doc)
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+
+    }
+
+}
+
+
+
---
+++ HelloWorld/src/com/firstjavaprogram/Second.java
@@ -0,0 +1,13 @@
+package com.firstjavaprogram;
+
+public class Second {
+
+
+    public static void main(String[] args){
+        int x = 5;
+        System.out.println(x+5);
+    }
+
+
+
+}
